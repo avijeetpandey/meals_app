e3aaee0c9095c0df2a48278c03391ca7816f3522
Avijeet Pandey <avijeetpandey25@gmail.com>
2024-08-11 23:51:59 +0530

    add: google fonts

---
+++ lib/meals_app.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:meals_app/theme/theme_data.dart';
+
+class MealsApp extends StatelessWidget {
+  const MealsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme,
+      home: // Todo ...,
+    );
+  }
+}
